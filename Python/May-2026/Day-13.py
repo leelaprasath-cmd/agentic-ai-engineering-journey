95606bafa959c3149b88cc995847649874ff6abe
Leela Prasath S <leelaprasathscse2025@jerusalemengg.ac.in>
2026-05-31 22:27:28 +0530

    Create Day-13.py

---
+++ Python/May-2026/Day-13.py
@@ -0,0 +1,23 @@
+# ==========================================
+# DAY 13 — PYTHON MODULES PRACTICE
+# ==========================================
+
+import random
+import math
+
+print("===== PYTHON MODULES PRACTICE =====")
+
+# Random Module
+random_number = random.randint(1, 100)
+
+print("\nRandom Number:", random_number)
+
+# Math Module
+number = int(input("\nEnter a number: "))
+
+print("\nSquare Root :", math.sqrt(number))
+print("Power (Square) :", math.pow(number, 2))
+print("Ceiling Value :", math.ceil(number))
+print("Floor Value :", math.floor(number))
+
+print("\n===== PROGRAM COMPLETED =====")
